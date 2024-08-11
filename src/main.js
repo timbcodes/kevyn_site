1790d88986e1fef37c536002d44dee94aaf4af2e
Timothy Bradford <tbradford@firehousealerting.com>
2024-08-11 17:24:33 -0500

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,31 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+/**
+ * Import Custom Styles Below
+ */
+import "@/scss/fonts.scss";
+import "@/scss/normalize.scss";
+import "@/scss/UniversalStyles.scss";
+import "@/scss/mixins.scss";
+import "@/scss/variables.scss";
+import 'animate.css/animate.css';
+
+/**
+ * Import External Libraries Below
+*/
+import VueCookies from "vue3-cookies";
+import vClickOutside from "click-outside-vue3";
+import { Vue3Mq } from "vue3-mq";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueCookies)
+  .use(vClickOutside)
+  .use(Vue3Mq, {
+    preset: "devices", // default
+  });
+
+app.mount("#app");
